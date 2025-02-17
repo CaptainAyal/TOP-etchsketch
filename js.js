a1d16111601757f69a75496a10e529ee7c2636ba
CaptainAyal <ayalmesher@gmail.com>
2025-02-17 16:51:24 +0200

    Dynamically creates grid

--- js.js
+++ js.js
@@ -1,17 +1,23 @@
 let main = document.querySelector("#main")
 
-
-// row.classList.add("top")
-
-// let sub = document.createElement("div")
-// sub.classList.add("box")
+let sub = document.createElement("div")
+sub.classList.add("box")
 // sub.classList.add("sub")
 
 let create = function(num){
-    i=0
+    let i=0
     while (i<num) {
         let row = document.createElement("div")
-        row.classList.add("box")
+        row.classList.add("row")
+        
+        let ii= 0
+        while (ii<num){
+            let sub = document.createElement("div")
+            sub.classList.add("box")
+            row.appendChild(sub)
+            ii++
+        }
+
        main.appendChild(row) 
        i++
     }
